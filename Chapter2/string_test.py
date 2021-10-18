1302bf3b7d214001e629ca92539b1bea7f115c8d
Mika Vallin <mika.vallin@gmail.com>
2021-10-18 13:14:39 +0300

    Create some scripts

---
+++ Chapter2/string_test.py
@@ -0,0 +1,11 @@
+first_string = "This is a string."
+second_string = 'This is also a string.'
+quote_inside_string = "The language 'Python' is named after Monty Python, not the snake"
+second_quote_inside_string = 'I told my friend, "Python is my favorite language!"'
+third_quote_inside_string = "One of Python's strengths is its diverse and supportive community."
+
+print(first_string)
+print(second_string)
+print(quote_inside_string)
+print(second_quote_inside_string)
+print(third_quote_inside_string)
