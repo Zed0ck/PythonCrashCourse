13b37d994989857dc36fcc12750bdb0dcaaa799c
Mika Vallin <mika.vallin@gmail.com>
2021-10-21 21:53:32 +0300

    Add some exercises ffor modifying lists

---
+++ Chapter3/cars.py
@@ -0,0 +1,23 @@
+cars = ['bmw', 'audi', 'toyota', 'skoda']
+
+# Sort cars alphabetically
+# cars.sort()
+# print(cars)
+
+# Sort cars reverse alphabetical order
+# cars.sort(reverse=True)
+# print(cars)
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+cars.reverse()
+print(cars)
+
+print(str(len(cars)))
--- Chapter3/own_list.py
+++ Chapter3/own_list.py
@@ -5,3 +5,33 @@ message_three = f"I have owned a {cars[2].title()}."
 print(message_one)
 print(message_two)
 print(message_three)
+
+
+# Muutama extra harjoitus, listalle lisäämiseen ja poistamiseen
+cars.append('ford')
+print(cars)
+
+cars.insert(1, 'toyota')
+print(cars)
+
+del cars[1]
+print(cars)
+
+popped_car = cars.pop()
+print(cars)
+print(popped_car)
+
+first_car = cars.pop(0)
+print(f"The first car in the list was a {first_car.title()}.")
+
+cars.remove('bmw')
+print(cars)
+
+cars.append('bmw')
+cars.append('tesla')
+print(cars)
+
+too_expensive = 'tesla'
+cars.remove(too_expensive)
+print(cars)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
