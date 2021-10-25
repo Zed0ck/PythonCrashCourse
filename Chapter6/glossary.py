44bfa96bd4a3459e4281f94f4e4cb1fc08a71a4a
Mika Vallin <mika.vallin@gmail.com>
2021-10-25 19:30:39 +0300

    Add dictionary exercises

---
+++ Chapter6/glossary.py
@@ -0,0 +1,7 @@
+glossary = {
+    'luuppi' : 'Luuppi mikä helpottaa koodin uudelleen käyttämistä',
+    'ehtolause' : 'If lause millä tarkistetaan, täyttyykö annettu ehto',
+}
+
+print(f"Luuppi\n\t{glossary['luuppi']}")
+print(f"Ehtolause\n\t{glossary['ehtolause']}")
---
+++ Chapter6/person.py
@@ -0,0 +1,5 @@
+person_0 = {'first_name' : 'sanni', 'last_name' : 'vallin', 'age': 5, 'city' : 'tampere'}
+print(person_0['age'])
+print(person_0['city'])
+print(person_0['first_name'])
+print(person_0['last_name'])
