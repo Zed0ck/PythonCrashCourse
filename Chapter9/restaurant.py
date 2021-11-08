8b5824bea880f0039a19a66108d8d2bff8887bc0
Mika Vallin <mika.vallin@gmail.com>
2021-11-08 22:48:55 +0200

    Add classes and instances exercises

--- Chapter9/restaurant.py
+++ Chapter9/restaurant.py
@@ -3,7 +3,19 @@ class Restaurant:
     def __init__(self, restaurant_name, restaurant_type):
         self.restaurant_name = restaurant_name
         self.restaurant_type = restaurant_type
+        self.number_served = 0
 
+    def read_number_served(self):
+        """Print number_served."""
+        print(f"\n{self.number_served} numbers served!")
+
+    def set_number_served(self, number):
+        """Set given number as number served."""
+        self.number_served = number
+    
+    def increment_number_served(self, increment_number):
+        """Add given increment to served number."""
+        self.number_served += increment_number
 
 my_restaurant = Restaurant('mc donalds', 'fast food')
 your_restaurant = Restaurant('dennis', 'pizza')
@@ -11,3 +23,10 @@ moms_restaurant = Restaurant('plevna', 'fine dinning')
 print(f"{my_restaurant.restaurant_name.title()} is a {my_restaurant.restaurant_type} restaurant.")
 print(f"{your_restaurant.restaurant_name.title()} is a {your_restaurant.restaurant_type} restaurant.")
 print(f"{moms_restaurant.restaurant_name.title()} is a {moms_restaurant.restaurant_type} restaurant.")
+
+my_restaurant.read_number_served()
+my_restaurant.set_number_served(6)
+my_restaurant.read_number_served()
+my_restaurant.increment_number_served(1)
+my_restaurant.read_number_served()
+
--- Chapter9/users.py
+++ Chapter9/users.py
@@ -4,13 +4,23 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.role = role
+        self.login_attempts = 0
 
     def describe_user(self):
         """Describes user"""
         print(f"\n\nFirst name: {self.first_name.title()}\nLast name: {self.last_name.title()}\nRole: {self.role}")
     
     def greet_user(self):
+        """Print greetings."""
         print(f"\nHello {self.first_name.title()} {self.last_name.title()}!")
+    
+    def increment_login_attempts(self):
+        """Add one to login attempts."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset login attempts to 0."""
+        self.login_attempts = 0
 
 user_one = User('albert', 'einstein', 'scientist')
 user_two = User('teemu', 'selanne', 'hockey player')
@@ -21,3 +31,11 @@ user_two.describe_user()
 user_two.greet_user()
 user_three.describe_user()
 user_three.greet_user()
+
+print(user_one.login_attempts)
+user_one.increment_login_attempts()
+print(user_one.login_attempts)
+user_one.increment_login_attempts()
+print(user_one.login_attempts)
+user_one.reset_login_attempts()
+print(user_one.login_attempts)
