daf01f54a043a5fa640758db7d58b2ca00206cf0
Mika Vallin <mika.vallin@gmail.com>
2021-11-10 20:35:49 +0200

    Adds exercises using modules

--- Chapter9/restaurant.py
+++ Chapter9/restaurant.py
@@ -1,3 +1,5 @@
+"""Classes that can be used as representing restaurants."""
+
 class Restaurant:
     """Class for restaurants"""
     def __init__(self, restaurant_name, restaurant_type):
@@ -26,20 +28,3 @@ class IceCreamStand(Restaurant):
         print("We have flavors:") 
         for flavor in self.flavors:
             print(f"\t{flavor.title()}")
-
-my_restaurant = Restaurant('mc donalds', 'fast food')
-your_restaurant = Restaurant('dennis', 'pizza')
-moms_restaurant = Restaurant('plevna', 'fine dinning')
-print(f"{my_restaurant.restaurant_name.title()} is a {my_restaurant.restaurant_type} restaurant.")
-print(f"{your_restaurant.restaurant_name.title()} is a {your_restaurant.restaurant_type} restaurant.")
-print(f"{moms_restaurant.restaurant_name.title()} is a {moms_restaurant.restaurant_type} restaurant.")
-
-my_restaurant.read_number_served()
-my_restaurant.set_number_served(6)
-my_restaurant.read_number_served()
-my_restaurant.increment_number_served(1)
-my_restaurant.read_number_served()
-ice_cream_restaurant = IceCreamStand('valio', 'dessert bar')
-print(ice_cream_restaurant.restaurant_name.title())
-ice_cream_restaurant.get_flavors()
-
