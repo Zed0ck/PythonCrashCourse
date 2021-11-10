91d9e748238cd550897c4235fab4fd00b94de675
Mika Vallin <mika.vallin@gmail.com>
2021-11-10 19:58:55 +0200

    Add import class lessons

--- Chapter9/car.py
+++ Chapter9/car.py
@@ -1,3 +1,5 @@
+"""A class that can be used to represent a car."""
+
 class Car:
     """A simple attempt to represent a car."""
 
@@ -28,10 +30,25 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
-my_new_car.update_odometer(42)
-my_new_car.read_odometer()
-my_new_car.increment_odometer(100)
-my_new_car.read_odometer()
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        
+        print(f"This car can go about {range} miles on a full charge.")
+    
+class ElectricCar(Car):
+    """Models aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
--- Chapter9/electric_car.py
+++ Chapter9/electric_car.py
@@ -1,32 +1,6 @@
-class Car:
-    """A simple attempt to represent a car."""
+"""A set of classes that can be used to represent electric cars."""
 
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-    
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        """Set the odometer reading to the given value."""
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
+from car import Car
 
 class ElectricCar(Car):
     """Represent aspect of a car, specific to electric vehicles."""
@@ -53,7 +27,3 @@ class Battery:
 
         print(f"This car can go about {range} miles on a full charge.")
 
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.descibe_battery()
-my_tesla.battery.get_range()
