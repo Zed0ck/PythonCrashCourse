6626e91936c8c4973ae5a327e5e4b3330e7f80cf
Mika Vallin <mika.vallin@gmail.com>
2021-11-16 20:16:02 +0200

    Add exercises with exceptions

---
+++ Chapter10/addition.py
@@ -0,0 +1,16 @@
+print("Give me two numbers and I add them together.")
+print("Enter 'q' to quit")
+
+while True:
+    number_one = input("\nFirst number: ")
+    if number_one == 'q':
+        break
+    number_two = input("Second number: ")
+    if number_two == 'q':
+        break
+    try:
+        answer = number_one + number_two
+    except ValueError:
+        print("You should give me two numbers.")
+    else:
+        print(answer)
